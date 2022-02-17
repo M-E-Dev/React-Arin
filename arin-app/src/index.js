45264a24ce469e3bd7d60ab617642a4fadcd3414
Muhammed Eroğlu <llofmejb@gmail.com>
2022-02-17 11:22:25 +0300

    2

--- arin-app/src/index.js
+++ arin-app/src/index.js
@@ -16,7 +16,7 @@ function App() {
                     {/* <img src="..." className="card-img-top" alt="..."> */}
                         <div className="card-body">
                             
-                            <h5 className="card-title">Card title</h5>
+                            <h5 className="card-title">Card title1</h5>
                             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                         </div>
@@ -24,7 +24,7 @@ function App() {
                 <div className="card">
                     {/* <img src="..." className="card-img-top" alt="..."> */}
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
+                            <h5 className="card-title">Card title2</h5>
                             <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
                             <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                         </div>
@@ -32,7 +32,7 @@ function App() {
                 <div className="card">
                     {/* <img src="..." className="card-img-top" alt="..."> */}
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
+                            <h5 className="card-title">Card title3</h5>
                             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                             <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                         </div>
