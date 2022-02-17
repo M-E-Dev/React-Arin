6638f90b0630285af8d03bcaac5f6357194e05c2
Muhammed Eroğlu <llofmejb@gmail.com>
2022-02-17 13:11:35 +0300

    2

--- arin-app/src/index.js
+++ arin-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Card from './components/card';
 
 //----------------JSX--------------------
 const names = ["Harry", "Hermione", "Ron"];
@@ -11,47 +12,13 @@ function App() {
       {/* <button type="button" style={{padding: "10px", color: "white", backgroundColor: "red", border: "2px solid blue"}}>jsx Button</button> */}
       {/* <h1 className="display-1">Display 1</h1> */}
       <div className="card-group">
-        <div className="card">
-          {/* <img src="..." className="card-img-top" alt="..."> */}
-          <div className="card-body">
-            <h5 className="card-title">Card title1</h5>
-            <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          {/* <img src="..." className="card-img-top" alt="..."> */}
-          <div className="card-body">
-            <h5 className="card-title">Card title2</h5>
-            <p className="card-text">
-              This card has supporting text below as a natural lead-in to
-              additional content.
-            </p>
-            <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          {/* <img src="..." className="card-img-top" alt="..."> */}
-          <div className="card-body">
-            <h5 className="card-title">Card title3</h5>
-            <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </p>
-            <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </p>
-          </div>
-        </div>
+
+        <Card cardTitle="Fenerbahçe"/>
+
+        <Card cardTitle="Galatasaray"/>
+
+        <Card cardTitle="Beşiktaş"/>
+
       </div>
     </div>
   );
