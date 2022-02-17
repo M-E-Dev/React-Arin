77d3498f20eea9e62a0fdb58257f36d5bb6335b9
Muhammed Eroğlu <llofmejb@gmail.com>
2022-02-17 12:20:08 +0300

    2

--- arin-app/src/index.js
+++ arin-app/src/index.js
@@ -1,45 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 //----------------JSX--------------------
 const names = ["Harry", "Hermione", "Ron"];
 
-
 function App() {
-    return (
-        <div>
-            {/* {names.map(name => (<h1>{name}</h1>))} */}
-            {/* <button type="button" style={{padding: "10px", color: "white", backgroundColor: "red", border: "2px solid blue"}}>jsx Button</button> */}
-            {/* <h1 className="display-1">Display 1</h1> */}
-            <div className="card-group">
-                <div className="card">
-                    {/* <img src="..." className="card-img-top" alt="..."> */}
-                        <div className="card-body">
-                            
-                            <h5 className="card-title">Card title1</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                </div>
-                <div className="card">
-                    {/* <img src="..." className="card-img-top" alt="..."> */}
-                        <div className="card-body">
-                            <h5 className="card-title">Card title2</h5>
-                            <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                </div>
-                <div className="card">
-                    {/* <img src="..." className="card-img-top" alt="..."> */}
-                        <div className="card-body">
-                            <h5 className="card-title">Card title3</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                </div>
-            </div>
+  return (
+    <div>
+      {/* {names.map(name => (<h1>{name}</h1>))} */}
+      {/* <button type="button" style={{padding: "10px", color: "white", backgroundColor: "red", border: "2px solid blue"}}>jsx Button</button> */}
+      {/* <h1 className="display-1">Display 1</h1> */}
+      <div className="card-group">
+        <div className="card">
+          {/* <img src="..." className="card-img-top" alt="..."> */}
+          <div className="card-body">
+            <h5 className="card-title">Card title1</h5>
+            <p className="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
         </div>
-    );
+        <div className="card">
+          {/* <img src="..." className="card-img-top" alt="..."> */}
+          <div className="card-body">
+            <h5 className="card-title">Card title2</h5>
+            <p className="card-text">
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          {/* <img src="..." className="card-img-top" alt="..."> */}
+          <div className="card-body">
+            <h5 className="card-title">Card title3</h5>
+            <p className="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 // class App extends React.Component {
 //     render () {
@@ -47,8 +62,18 @@ function App() {
 //     }
 // }
 
+ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-)
+// const kuzens = ["Özer", "Hasan", "Onur"];
+
+// function KuzenYaz() {
+//   return (
+//     <div>
+//       {kuzens.map((kuzen) => (
+//         <h1>{kuzen}</h1>
+//       ))}
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<KuzenYaz />, document.getElementById("root"));
