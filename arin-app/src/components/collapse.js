b0390e2e68439dd982e2d81c622299b3814b9525
Muhammed Eroğlu <llofmejb@gmail.com>
2022-03-15 23:08:15 +0300

    2

--- arin-app/src/components/collapse.js
+++ arin-app/src/components/collapse.js
@@ -1,18 +1,23 @@
 import React from "react";
 
-
 const Collapse = (props) => {
   console.log(props.children);
   return (
     <div className="m-2">
+      <a
+        className="btn btn-primary w-100"
+        data-bs-toggle="collapse"
+        href={"#".concat(props.href)}
+        role="button"
+        aria-expanded="false"
+        aria-controls="collapseExample"
+      >
+        Link with href
+      </a>
 
-  <a className="btn btn-primary w-100" data-bs-toggle="collapse" href={"#".concat(props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
-    Link with href
-  </a>
-
-<div className="collapse show" id={props.href}>
-    {props.children}
-</div>
+      <div className="collapse show" id={props.href}>
+        {props.children}
+      </div>
     </div>
   );
 };
