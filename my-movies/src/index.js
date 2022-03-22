edca67ab898b05efe2ef4d7e2709bf92117e26d6
Muhammed Eroğlu <llofmejb@gmail.com>
2022-03-22 21:57:08 +0300

    6

--- my-movies/src/index.js
+++ my-movies/src/index.js
@@ -1,2 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+// App.js buraya da gömülebilir, ayrı yapıp import ettik
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
