8397223558af7d4042d208ab6d46d2007d673da9
Muhammed Eroğlu <llofmejb@gmail.com>
2022-03-23 13:11:18 +0300

    1

--- my-movies/src/components/App.js
+++ my-movies/src/components/App.js
@@ -1,10 +1,49 @@
 import React from "react";
 
-const App = () => {
-    return (
-        <h1>My Movies</h1>
-    )
+class App extends React.component {
+    
+    state = {
+
+        movies : [
+          {
+            id: 1,
+            name: "The Flash",
+            rating: 8.3,
+            overview:
+              "This is a wider card with supporting text below as a natural lead-in to additional content",
+            imageURL:
+              "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lJA2RCMfsWoskqlQhXPSLFQGXEJ.jpg",
+          },
+          {
+            id: 2,
+            name: "Interstellar",
+            rating: 9.4,
+            overview:
+              "This is a wider card with supporting text below as a natural lead-in to additional content",
+            imageURL:
+              "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xbiycuc84TrieEWwkkuH2hoEa9S.jpg",
+          },
+          {
+            id: 3,
+            name: "Arrow",
+            rating: 6.7,
+            overview:
+              "This is a wider card with supporting text below as a natural lead-in to additional content",
+            imageURL:
+              "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg",
+          },
+        ]
+    }
+
+  render() {
+      return (<h1>My Movies</h1>);
+    };
 }
 
+//Fonsiyonel component versiyonu
+// const App = () => {
+//     return (<h1>My Movies</h1>)
+// }
 
 export default App;
+
