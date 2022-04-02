bbb301a03b20a790f645b989249ee9e0b74dce3b
Muhammed Eroğlu <llofmejb@gmail.com>
2022-04-02 22:28:19 +0300

    2

--- my-movies/src/components/SearchBar.js
+++ my-movies/src/components/SearchBar.js
@@ -2,14 +2,27 @@ import React from "react";
 
 class SearchBar extends React.Component {
 
+    state = {
+        searchQuery: ""
+    }
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+    }
+
     render(){
 
         return(
 
-            <form>
+            <form onSubmit={this.handleFormSubmit}>
                 <div className="form-row mb-5 mt-5">
                     <div className="col-12">
-                        <input type="text" className="form-control" placeholder="Search a movie..."></input>
+                        <input
+                        // value="Matrix"   --- value kullnırsak kontrol edemeyiz
+                        value={this.state.searchQuery}
+                        // onChange={(event) => console.log(event.target.value)}
+                        onChange={(event) => this.setState({searchQuery: event.target.value})}
+                        type="text" className="form-control" placeholder="Search a movie..."></input>
                     </div>
                 </div>
             </form>
