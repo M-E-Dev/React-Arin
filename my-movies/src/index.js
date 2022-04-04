e42061b97419cdb1bc8dddfcd35030f39023d705
Muhammed Eroğlu <llofmejb@gmail.com>
2022-04-04 17:36:03 +0300

    3

--- my-movies/src/index.js
+++ my-movies/src/index.js
@@ -5,25 +5,3 @@ import App from "./components/App"
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById("root"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// import App from './components/App';
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
