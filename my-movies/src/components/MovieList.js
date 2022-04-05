f70b30a8e6f741ad2907d0056cb69f35aade5efe
Muhammed Eroğlu <llofmejb@gmail.com>
2022-04-05 17:43:44 +0300

    3

--- my-movies/src/components/MovieList.js
+++ my-movies/src/components/MovieList.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-const MovieList = () => {
+const MovieList = (props) => {
     return(
         <div className="row">
         
         {props.movies.map((movie) => (
-        <div className="col-lg-4" key={movie.id}>
+        <div className="col-lg-4">
           <div className="card mb-4 shadow-sm">
               <img src={movie.imageURL} className="card-img-top" alt="Sample-Pic"></img>
               <div className="card-body">
-                    <h5 className="card-title">{movie.name}</h5>
-                    <p className="card-text">{movie.overview}</p>
+                    <h5 className="card-title">{}</h5>
+                    <p className="card-text">{}</p>
                     <div className="d-flex justify-content-between align-items-center">
-                        <button type="button" onClick={() => props.deleteMovieProp(movie)} className="btn btn-md btn-outline-danger">Delete</button>
-                        <h2><span className="badgebadge-info">{movie.rating}</span></h2>
+                        <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
+                        <h2><span className="badgebadge-info">{}</span></h2>
                     </div>
               </div>
           </div>
