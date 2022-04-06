b8df5edbad4736f5ac482e49bf92f5a7d5327dba
Muhammed Eroğlu <llofmejb@gmail.com>
2022-04-06 14:19:19 +0300

    3

--- my-movies/src/components/SearchBar.js
+++ my-movies/src/components/SearchBar.js
@@ -2,27 +2,36 @@ import React from "react";
 
 class SearchBar extends React.Component {
 
-    handleFormSubmit = (event) => {
-        event.preventDefault()
-    }
     
-    render() {
-        return(
-            <form onSubmit={this.handleFormSubmit}>
-                <div className="form-row mb-5 mt-5">
-                    <div className="col-12 px-2">
-                        <input
-                        // value="Matrix"   --- value kullnırsak kontrol edemeyiz
-                        // value={this.state.searchQuery}
-                        // onChange={(event) => console.log(event.target.value)}
-                        // onChange={(event) => this.setState({searchQuery: event.target.value})}
-                        onChange={this.props.searchMovieProp}
-                        type="text" className="form-control" placeholder="Search a movie..."></input>
-                    </div>
-                </div>
-            </form>
-        )
-    }
+    // state = {
+    //     searchQuery: ""
+    // }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <div className="form-row mb-5 mt-5">
+          <div className="col-12 px-2">
+            <input
+              // value="Matrix"   --- value kullnırsak kontrol edemeyiz
+              // value={this.state.searchQuery}
+              // onChange={(event) => console.log(event.target.value)}
+              // onChange={(event) => this.setState({searchQuery: event.target.value})}
+              onChange={this.props.searchMovieProp}
+              type="text"
+              className="form-control"
+              placeholder="Search a movie..."
+            ></input>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
+
 
 export default SearchBar;
