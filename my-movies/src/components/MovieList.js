0180366f9f9832c6fd30335f9830be9c8dceab3f
Muhammed Eroğlu <llofmejb@gmail.com>
2022-04-16 12:29:13 +0300

    5

--- my-movies/src/components/MovieList.js
+++ my-movies/src/components/MovieList.js
@@ -1,16 +1,33 @@
 import React from "react";
 
 const MovieList = (props) => {
+
+    // function handleClick(event) {
+    //   console.log(event.pageY)
+    // }
+
+    const truncateOverview = (string, maxLength) => {
+      // if (!string) {
+      //   return null
+      // } else if (string.length <= maxLength){
+      //   return string
+      // } else return `${string.substring(0, maxLength)} ...`
+      // // ilk return alınacağı için else falan gerekmez!!
+      if (!string) return null;
+      if (string.length <= maxLength) return string;
+      return `${string.substring(0, maxLength)} ...`
+    }
+
     return(
         <div className="row">
         
-        {props.movies.map((movie) => (
-        <div className="col-lg-4" key={movie.id}>
+        {props.movies.map((movie, i) => (
+        <div className="col-lg-4" key={i}>
           <div className="card mb-4 shadow-sm">
               <img src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`} className="card-img-top" alt="Sample-Pic"></img>
               <div className="card-body">
                     <h5 className="card-title">{movie.title}</h5>
-                    <p className="card-text">{movie.overview}</p>
+                    <p className="card-text">{truncateOverview(movie.overview, 120)}</p>
                     <div className="d-flex justify-content-between align-items-center">
                         <button type="button" onClick={() => props.deleteMovieProp(movie)} className="btn btn-md btn-outline-danger">Delete</button>
                         <h2><span className="badgebadge-info">{movie.vote_average}</span></h2>
